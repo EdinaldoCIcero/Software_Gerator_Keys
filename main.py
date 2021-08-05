e5534f92eaaa0783fa2db474dbfb86a75c98411d
EdinaldoCicero <68930411+EdinaldoCIcero@users.noreply.github.com>
2021-08-05 18:24:18 -0300

    Main_1
    Inicio da criação do software

---
+++ main.py
@@ -0,0 +1,97 @@
+import PySimpleGUI as sg
+from pytube import YouTube
+import re 
+import os
+import sys
+
+
+
+
+sg.theme("DarkBrown4")
+
+Cores = [ "#ffffff"  , "#0091ff" , "#242424" , "#ba4747" ]
+
+
+
+class AppClass():
+    def __init__(self):
+        
+        self.ImagemLayer = [
+        [sg.Canvas(canvas=None, background_color=Cores[0] ,size=(150, 300) )],
+        #[sg.Image(filename = None,size = (None, None))]
+        ]
+
+
+        self.lay = [
+                
+                #---------------------------------------------------------------------------------------
+                #[sg.Image(filename = None,size = (None, None))]
+                [sg.Canvas(canvas=None, background_color=Cores[0],size=(720, 120)) ,],
+
+
+                [
+                sg.Text("URL do video",text_color=Cores[3] , size=( 10 , 1))
+                ],
+                #---------------------------------------------------------------------------------------
+
+                #---------------------------------------------------------------------------------------
+                [sg.HorizontalSeparator(color=Cores[3],key="separador002")],
+
+                
+                #---------------------------------------------------------------------------------------
+                #[sg.Canvas(canvas=None, background_color=Cores[2],size=(70, 5)) ,],
+                #---------------------------------------------------------------------------------------
+
+                [
+                sg.Canvas(canvas=None, background_color=Cores[2],size=(70, 5)) , 
+                sg.Checkbox("360" , text_color=Cores[3], key="360" ),
+
+                sg.Canvas(canvas=None, background_color=Cores[2],size=(70, 5)) , 
+                sg.Checkbox("720" , text_color=Cores[3], key="720" )
+                ],
+
+
+                #---------------------------------------------------------------------------------------
+                [sg.HSeparator()],
+                
+                #---------------------------------------------------------------------------------------
+                [sg.Output(size=(65,20),  background_color=Cores[3] ,key="out") ],
+
+                [
+                #sg.HSeparator(),
+                sg.Button("Download_video", button_color=Cores[3], key="down", size=(21,1)),
+                
+                ],
+
+                ]
+
+
+        self.layout = [
+                [self.lay  
+
+                #self.ImagemLayer
+                #sg.Column(self.ImagemLayer),
+                #sg.VSeparator(),
+                ],
+
+                ]
+
+
+        self.janela = sg.Window("Gerator_Keys",background_color=Cores[2] , size=(720,480)).layout(self.layout)
+
+
+
+    def Update(self):
+        while True:
+            self.events, self.values = self.janela.Read()
+
+            if self.values == sg.WIN_CLOSED or self.values == "Sair":
+                break
+
+
+
+
+
+
+app = AppClass()
+app.Update()
