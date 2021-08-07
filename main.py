d7851a9478c4e0bb6ee7486490d56b82c063fafe
EdinaldoCicero <68930411+EdinaldoCIcero@users.noreply.github.com>
2021-08-06 21:03:21 -0300

    software quase pronto
    V_0.1.0 , quase pronto para finalizar e exportar para exe, os elementos já estão nos seus lugares só falta personalização dos proprios!.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import PySimpleGUI as sg
-from pytube import YouTube
+from random import randint
 import re 
 import os
 import sys
+import string 
+import random
 
 
 
-
-sg.theme("DarkBrown4")
+sg.theme("DarkGrey3")
 
 Cores = [ "#ffffff"  , "#0091ff" , "#242424" , "#ba4747" ]
 
@@ -15,85 +16,99 @@ Cores = [ "#ffffff"  , "#0091ff" , "#242424" , "#ba4747" ]
 
 class AppClass():
     def __init__(self):
-        
-        self.ImagemLayer = [
-        [sg.Canvas(canvas=None, background_color=Cores[0] ,size=(150, 300) )],
-        #[sg.Image(filename = None,size = (None, None))]
-        ]
+
+        self.list_senha = []
+        self.list_senha_for_save = []
 
 
         self.lay = [
-                
                 #---------------------------------------------------------------------------------------
-                #[sg.Image(filename = None,size = (None, None))]
-                [sg.Canvas(canvas=None, background_color=Cores[0],size=(720, 120)) ,],
-
-
+                #[sg.Image(filename = None,size = (720, 160))]
+                [sg.Canvas(canvas=None, background_color=Cores[0],size=(720, 160)) ,],
+                #---------------------------------------------------------------------------------------
                 [
                 sg.Text("URL do video",text_color=Cores[3] , size=( 10 , 1))
                 ],
-                #---------------------------------------------------------------------------------------
 
                 #---------------------------------------------------------------------------------------
                 [sg.HorizontalSeparator(color=Cores[3],key="separador002")],
 
-                
                 #---------------------------------------------------------------------------------------
-                #[sg.Canvas(canvas=None, background_color=Cores[2],size=(70, 5)) ,],
-                #---------------------------------------------------------------------------------------
-
                 [
-                sg.Canvas(canvas=None, background_color=Cores[2],size=(70, 5)) , 
-                sg.Checkbox("360" , text_color=Cores[3], key="360" ),
-
-                sg.Canvas(canvas=None, background_color=Cores[2],size=(70, 5)) , 
-                sg.Checkbox("720" , text_color=Cores[3], key="720" )
+                sg.Canvas(canvas=None, background_color=Cores[2],size=(50, 5)),
+                sg.Slider(range = (0, 100),size=(60 , 10) , default_value = 0 ,orientation="horizontal" , key="sizekey")
                 ],
-
 
                 #---------------------------------------------------------------------------------------
                 [sg.HSeparator()],
                 
                 #---------------------------------------------------------------------------------------
-                [ sg.Canvas(canvas=None, background_color=Cores[2],size=(200, 5)),
-                sg.Multiline(default_text="",autoscroll=False , size=(30,0),background_color="#e0e0e0",text_color=None ,key ="Mult")],
-                #[sg.Output(size=(40,5),  background_color=Cores[3] ,key="out") ],
+                [sg.Canvas(canvas=None, background_color=Cores[2],size=(10, 20))],
 
+                #---------------------------------------------------------------------------------------
                 [
-                #sg.HSeparator(),
-                sg.Button("Download_video", button_color=Cores[3], key="down", size=(21,1)),
-                
+                sg.Canvas(canvas=None, background_color=Cores[2],size=(178, 5)),
+                sg.Button("gerar senha ", button_color=None, key="down", size=(30,1))
                 ],
 
+                #---------------------------------------------------------------------------------------
+                [sg.Canvas(canvas=None, background_color=Cores[2],size=(10, 20))],
+
+                #---------------------------------------------------------------------------------------
+                [ 
+                sg.Canvas(canvas=None, background_color=Cores[2],size=(170, 5)) ,
+                sg.InputText(size=(40,2) , background_color="#e0e0e0", pad=(0 ,(0 , 10 )) , key="saida")
+                ],
+
+                #---------------------------------------------------------------------------------------
+
+                [sg.Canvas(canvas=None, background_color=Cores[2],size=(100, 42))],
+                [sg.HSeparator()],
+                #---------------------------------------------------------------------------------------
+                [
+                sg.Canvas(canvas=None, background_color=Cores[2],size=(500, 2)),
+                #sg.FolderBrowse("Buscar" , key="path") ,
+                sg.Button("salvar senha", button_color=None, key="save", size=(21,1))
+                ],
+
+                [
+                sg.Canvas(canvas=None, background_color=Cores[2],size=(200, 2)),
+                sg.Text("criado por:EdinaldoCicero/Átomos Games Studios",auto_size_text= 2 , text_color=Cores[0] ,size=(30,1),s=(1,1))]
                 ]
 
 
         self.layout = [
-                [self.lay  
+                    [self.lay],
 
-                #self.ImagemLayer
-                #sg.Column(self.ImagemLayer),
-                #sg.VSeparator(),
-                ],
-
-                ]
+                    ]
 
 
-        self.janela = sg.Window("Gerator_Keys",background_color=Cores[2] , size=(720,480)).layout(self.layout)
+        self.janela = sg.Window("Gerator_Keys",background_color=None , size=(720,480)).layout(self.layout)
+
+        pass
 
 
 
     def Update(self):
         while True:
             self.events, self.values = self.janela.Read()
-
+            LINES = ("--"*20)
             if self.values == sg.WIN_CLOSED or self.values == "Sair":
                 break
 
             if self.events == "down":
-                self.values["Mult"] = "alfagouangowug"
+                senha_final = string.ascii_letters + string.digits + '!@#$%¨&*()-_=+[]<>,.;:?'
+                ran = random.SystemRandom()
+                self.janela["saida"].update( "".join(ran.choice(senha_final) for i in range( int( self.values["sizekey"] ) ) ))
+                
 
+            if self.events == "save":
+                self.list_senha.append(self.values["saida"])
 
+                with open( "senha_gerada.txt" , "w" ) as openedfile:
+                    openedfile.write( LINES + "\n" + "SENHA CRAIDA : " + self.list_senha[0] + "\n" + LINES )
+                    #print( self.list_senha )
+                    self.list_senha = []
 
 
 app = AppClass()
